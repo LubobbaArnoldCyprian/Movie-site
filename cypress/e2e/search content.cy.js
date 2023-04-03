6d8d2f46e895cde772923ec87e23de5f2c4b5d10
LubobbaArnoldCyprian <lubobba@outlook.com>
2023-04-03 22:59:21 +0300

    clean up search feature

--- cypress/e2e/search content.cy.js
+++ cypress/e2e/search content.cy.js
@@ -1,39 +1,30 @@
+/// <reference types="cypress" />
 describe('search content & check content detail page', ()=>{
 
-/*
-     it("test", function(){
-        cy.fixture(example).then((x)=>{
-            cy.wrap(x).then((fetchinfo)=>{
-
-                cy.visit('url');
-
-
-            })
-        })
-     }) */
 
     before(function(){
-        cy.fixture('example.json').then(function(data){
-            this.regdata=data
+        cy.fixture('example').then((access) =>{
+            cy.visit(access.baseurl)
+
         })
     })
 
     it('Test case one', function(){
-        cy.visit("https://www3.zoechip.com/")
-        cy.get('#onesignal-slidedown-cancel-button').click()
 
-        // let expName = 'Mr robot'
-        let search = cy.get('#search > .search-content > form > .form-control')
-        search.click()
-        search.type('Mr Robot')
-        cy.wait(3000)
+            cy.get('#onesignal-slidedown-cancel-button').click()
 
-        cy.get('#search > .search-content > .nav >')
-            .eq(0)
-            .click()
-        cy.get('.heading-name > a')
-            .should('be.visible')
-            .should('have.text', 'Mr. Robot')     
+            // let expName = 'Mr robot'
+            let search = cy.get('#search > .search-content > form > .form-control')
+            search.click()
+            search.type('Mr Robot')
+            cy.wait(3000)
+    
+            cy.get('#search > .search-content > .nav >')
+                .eq(0)
+                .click()
+            cy.get('.heading-name > a')
+                .should('be.visible')
+                .should('have.text', 'Mr. Robot')   
 
     })
 
